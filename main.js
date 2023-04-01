7370bb46f1e2ede4f88ed6706787a5c6ef6b22f4
mohammadsdq <97308791+Mohammadsdq@users.noreply.github.com>
2023-04-01 23:04:32 +0330

    updated
    + localstorage
    + new style
    + fixes

--- main.js
+++ main.js
@@ -1,52 +1,53 @@
 let userScore = 0;
 let aiScore = 0;
-
 const chooses = ["rock", "paper", "scissors"];
 
-// user Choose 
-const userChoose = prompt("choose one more !! \n  rock, paper, scissors");
-if (userChoose === "rock" || userChoose === "paper" || userChoose === "scissors") {
-    document.getElementById("UserChose").innerHTML = `User Choose : ${userChoose}`;
+// user Choice 
+const userChoose = document.querySelector("input");
+const userChoice = document.querySelector("#UserChose");
+const AIChoice = document.querySelector("#AIChose");
+const WinLose = document.querySelector("#WinLose")
+const btn = document.querySelector("button");
 
-    // camputer choose 
+btn.addEventListener("click", score)
+
+function score() {
+    // computer choice
     const randomNumber = Math.floor(Math.random() * 3);
-    const camputerChoose = chooses[randomNumber];
-
-    document.getElementById("AIChose").innerHTML = `AI choose : ${camputerChoose}`;
-
-    // Win & Lose 
-    if (userChoose === camputerChoose) {
-        document.getElementById("WinLose").innerHTML = "User&AI Equal !!";
-    } else if (userChoose === "rock") {
-        if (camputerChoose === "paper") {
-            document.getElementById("WinLose").innerHTML = "AI Win !";
-            aiScore++;
-        } else {
-            document.getElementById("WinLose").innerHTML = "User Win !";
-            userScore++;
-        }
-    } else if (userChoose === "paper") {
-        if (camputerChoose == "scissors") {
-            document.getElementById("WinLose").innerHTML = "AI Win !";
-            aiScore++;
-        } else {
-            document.getElementById("WinLose").innerHTML = "User Win !";
-            userScore++;
-        }
-    } else if (userChoose === "scissors") {
-        if (camputerChoose === "rock") {
-            document.getElementById("WinLose").innerHTML = "AI Win !";
-            aiScore++;
-        } else {
-            document.getElementById("WinLose").innerHTML = "User Win !";
-            userScore++;
-        }
+    const computerChoice = chooses[randomNumber];
+    if (userChoose.value === "rock" || userChoose.value === "paper" || userChoose.value === "scissors") {
+        userChoice.innerHTML = `UserChoice:${userChoose.value}`;
+        AIChoice.innerHTML = `AiChoice:${computerChoice}`
     }
 
-    // show score 
+    if (userChoose.value === computerChoice) {
+        WinLose.innerHTML = "User&AI Equal 😐!"
+    } else if (userChoose.value === "rock") {
+        if (computerChoice === "scissors") {
+            WinLose.innerHTML = "You Win🥳!"
+            userScore++;
+        } else {
+            WinLose.innerHTML = "Ai Win😪!"
+            aiScore++;
+        }
+    } else if (userChoose.value === "paper") {
+        if (computerChoice === "rock") {
+            WinLose.innerHTML = "You Win🥳!"
+            userScore++;
+        } else {
+            WinLose.innerHTML = "Ai Win😪!"
+            aiScore++;
+        }
+    } else if (userChoice.value === "scissors") {
+        if (computerChoice === "paper") {
+            WinLose.innerHTML = "You Win🥳!"
+            userScore++;
+        } else {
+            WinLose.innerHTML = "Ai Win😪!"
+            aiScore++;
+        }
+    }
+    
     document.getElementById("UserScore").innerHTML = `User Score : ${userScore}`;
     document.getElementById("AIScore").innerHTML = `AI Score : ${aiScore}`;
-
-} else {
-    document.getElementById("cheater").innerHTML = "hahaha \n Fuck You !!";
 }
